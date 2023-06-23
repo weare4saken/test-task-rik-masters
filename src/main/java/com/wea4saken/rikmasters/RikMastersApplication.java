0765e20b0194d0a580e2010e36a3a13db5aa9662
weare4saken <kondrashka91@gmail.com>
2023-06-23 14:31:24 +0700

    initial commit

---
+++ src/main/java/com/wea4saken/rikmasters/RikMastersApplication.java
@@ -0,0 +1,13 @@
+package com.wea4saken.rikmasters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RikMastersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RikMastersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wea4saken/rikmasters/RikMastersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wea4saken.rikmasters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RikMastersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
