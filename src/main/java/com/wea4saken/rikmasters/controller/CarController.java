8d6c26bb23eadb1a51f572a442700ceeb6c2e3c8
weare4saken <kondrashka91@gmail.com>
2023-06-24 03:10:39 +0700

    Add methods on CarController

--- src/main/java/com/wea4saken/rikmasters/controller/CarController.java
+++ src/main/java/com/wea4saken/rikmasters/controller/CarController.java
@@ -2,6 +2,7 @@ package com.wea4saken.rikmasters.controller;
 
 import com.wea4saken.rikmasters.dto.CarDto;
 import com.wea4saken.rikmasters.service.impl.CarService;
+import com.wea4saken.rikmasters.service.impl.DetailService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -21,25 +22,26 @@ import java.util.List;
 public class CarController {
 
     private final CarService carService;
+    private final DetailService detailService;
 
     @Operation(summary = "Добавление нового автомобиля", tags = "Автомобили",
-                responses = {@ApiResponse(responseCode = "201", description = "CREATED",
-                                            content = {@Content(mediaType = "application/json",
-                                            schema = @Schema(implementation = CarDto.class))})})
+            responses = {@ApiResponse(responseCode = "201", description = "CREATED",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = CarDto.class))})})
     @PostMapping
     public ResponseEntity<CarDto> addCar(@RequestBody CarDto carDto) {
         return ResponseEntity.status(HttpStatus.CREATED).body(carService.add(carDto));
     }
 
     @Operation(summary = "Редактирование сведений об автомобиле", tags = "Автомобили",
-                responses = {@ApiResponse(responseCode = "200", description = "OK",
-                                            content = {@Content(mediaType = "application/json",
-                                            schema = @Schema(implementation = CarDto.class))}),
-                            @ApiResponse(responseCode = "404", description = "Not Found",
-                                            content = @Content)})
+            responses = {@ApiResponse(responseCode = "200", description = "OK",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = CarDto.class))}),
+                    @ApiResponse(responseCode = "404", description = "Not Found",
+                            content = @Content)})
     @PatchMapping("/{vin}")
     public ResponseEntity<CarDto> updateCar(@PathVariable("vin") String vin,
-                                                  @RequestBody CarDto carDto) {
+                                            @RequestBody CarDto carDto) {
         return ResponseEntity.ok(carService.update(vin, carDto));
     }
 
@@ -50,22 +52,42 @@ public class CarController {
     }
 
     @Operation(summary = "Получение автомобиля по VIN-номеру", tags = "Автомобили",
-                responses = {@ApiResponse(responseCode = "200", description = "OK",
-                                            content = {@Content(mediaType = "application/json",
-                                            schema = @Schema(implementation = CarDto.class))}),
-                            @ApiResponse(responseCode = "404", description = "Not Found",
-                                            content = @Content)})
+            responses = {@ApiResponse(responseCode = "200", description = "OK",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = CarDto.class))}),
+                    @ApiResponse(responseCode = "404", description = "Not Found",
+                            content = @Content)})
     @GetMapping("/{vin}")
     public ResponseEntity<CarDto> getCar(@PathVariable("vin") String vin) {
         return ResponseEntity.ok(carService.get(vin));
     }
 
-    @Operation(summary = "Удалить деталь", tags = "Детали",
-                responses = {@ApiResponse(responseCode = "200", description = "OK", content = @Content),
-                            @ApiResponse(responseCode = "404", description = "Not Found", content = @Content)})
+    @Operation(summary = "Удалить автомобиль", tags = "Автомобили",
+            responses = {@ApiResponse(responseCode = "200", description = "OK", content = @Content),
+                    @ApiResponse(responseCode = "404", description = "Not Found", content = @Content)})
     @DeleteMapping("/{vin}")
     public ResponseEntity<?> deleteCar(@PathVariable("vin") String vin) {
         carService.delete(vin);
+        return ResponseEntity.ok().build();
+    }
+
+    @Operation(summary = "Установить владение автомобиля водителем", tags = "Автомобили",
+            responses = {@ApiResponse(responseCode = "200", description = "OK", content = @Content),
+                    @ApiResponse(responseCode = "404", description = "Not Found", content = @Content)})
+    @PostMapping("/{vin}/add-driver/{id}")
+    public ResponseEntity<?> addDriver(@PathVariable("vin") String vin,
+                                       @PathVariable("id") Long id) {
+        carService.addDriver(vin, id);
+        return ResponseEntity.ok().build();
+    }
+
+    @Operation(summary = "Установить деталь на автомобиль", tags = "Автомобили",
+            responses = {@ApiResponse(responseCode = "200", description = "OK", content = @Content),
+                    @ApiResponse(responseCode = "404", description = "Not Found", content = @Content)})
+    @PostMapping("/{vin}/add-detail/{type}")
+    public ResponseEntity<?> addDetail(@PathVariable("vin") String vin,
+                                       @PathVariable("type") String type) {
+        detailService.addDetail(vin, type);
         return ResponseEntity.ok().build();
     }
 }
